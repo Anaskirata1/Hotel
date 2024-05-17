3b86a7170ffde1bc7595121bae6812dbc37c5690
Anas.K <anaskirata2@gmail.com>
2024-05-17 11:14:50 -0700

    middleware

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,6 +5,13 @@
     @include('admin.css')
   </head>
   <body>
+    @if (session('delay_redirect'))
+    <script>
+        setTimeout(function() {
+            window.location.href = "{{ url('/') }}";
+        }, {{ session('delay_redirect') }});
+    </script>
+    @endif
    @include('admin.header')
    @include('admin.sidebar')
       <!-- Sidebar Navigation end-->
--- routes/web.php
+++ routes/web.php
@@ -33,44 +33,44 @@ Route::get('/', [AdminController::class, 'home']);
 
 Route::get('/home', [AdminController::class, 'index'])->name('home');
 
-Route::get('/create_room', [AdminController::class, 'create_room']);
+Route::get('/create_room', [AdminController::class, 'create_room'])->middleware(['auth', 'admin']);
 
-Route::post('/add_room', [AdminController::class, 'add_room']);
+Route::post('/add_room', [AdminController::class, 'add_room'])->middleware(['auth', 'admin']);
 
-Route::get('/view_room', [AdminController::class, 'view_room']);
+Route::get('/view_room', [AdminController::class, 'view_room'])->middleware(['auth', 'admin']);
 
-Route::get('/room_delete/{id}', [AdminController::class, 'room_delete']);
+Route::get('/room_delete/{id}', [AdminController::class, 'room_delete'])->middleware(['auth', 'admin']);
 
-Route::get('/room_update/{id}', [AdminController::class, 'room_update']);
+Route::get('/room_update/{id}', [AdminController::class, 'room_update'])->middleware(['auth', 'admin']);
 
-Route::post('/edit_room/{id}', [AdminController::class, 'edit_room']);
+Route::post('/edit_room/{id}', [AdminController::class, 'edit_room'])->middleware(['auth', 'admin']);
 
 Route::get('/room_details/{id}', [HomeController::class, 'room_details']);
 
-Route::post('/add_booking/{id}', [HomeController::class, 'add_booking']);
+Route::post('/add_booking/{id}', [HomeController::class, 'add_booking'])->middleware(['auth']);
 
-Route::get('/bookings', [AdminController::class, 'bookings']);
+Route::get('/bookings', [AdminController::class, 'bookings'])->middleware(['auth', 'admin']);
 
-Route::get('/book_delete/{id}', [AdminController::class, 'book_delete']);
+Route::get('/book_delete/{id}', [AdminController::class, 'book_delete'])->middleware(['auth', 'admin']);
 
-Route::get('/approve_book/{id}', [AdminController::class, 'approve_book']);
+Route::get('/approve_book/{id}', [AdminController::class, 'approve_book'])->middleware(['auth', 'admin']);
 
-Route::get('/rejected_book/{id}', [AdminController::class, 'rejected_book']);
+Route::get('/rejected_book/{id}', [AdminController::class, 'rejected_book'])->middleware(['auth', 'admin']);
 
-Route::get('print_pdf', [AdminController::class, 'print_pdf']);
+Route::get('print_pdf', [AdminController::class, 'print_pdf'])->middleware(['auth', 'admin']);
 
-Route::get('view_gallary', [AdminController::class, 'view_gallary']);
+Route::get('view_gallary', [AdminController::class, 'view_gallary'])->middleware(['auth', 'admin']);
 
-Route::post('upload_gallary', [AdminController::class, 'upload_gallary']);
+Route::post('upload_gallary', [AdminController::class, 'upload_gallary'])->middleware(['auth', 'admin']);
 
-Route::get('gallary_delete/{id}', [AdminController::class, 'gallary_delete']);
+Route::get('gallary_delete/{id}', [AdminController::class, 'gallary_delete'])->middleware(['auth', 'admin']);
 
-Route::post('contact', [HomeController::class, 'contact']);
+Route::post('contact', [HomeController::class, 'contact'])->middleware(['auth']);
 
 Route::get('contact_nav', [HomeController::class, 'contact_nav']);
 
-Route::get('all_messages', [AdminController::class, 'all_messages']);
+Route::get('all_messages', [AdminController::class, 'all_messages'])->middleware(['auth', 'admin']);
 
-Route::get('send_mail/{id}', [AdminController::class, 'send_mail']);
+Route::get('send_mail/{id}', [AdminController::class, 'send_mail'])->middleware(['auth', 'admin']);
 
-Route::post('mail/{id}', [AdminController::class, 'mail']);
+Route::post('mail/{id}', [AdminController::class, 'mail'])->middleware(['auth', 'admin']);
